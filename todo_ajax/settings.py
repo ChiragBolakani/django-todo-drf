58b8c7d28f82cfce613d2a6c64d41896d13dd12d
ChiragBolakani <cheragbolakani@gmail.com>
2024-03-19 02:17:56 +0530

    allowed hosts all

--- todo_ajax/settings.py
+++ todo_ajax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-49_m=27d84#z&%m=-ptxe4umpvu-dsa09er-bq^&8n*jzas52^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
